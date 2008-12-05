9e418de31f475b491aca0aced98fb331764c72d8
mat <matthew.kelcey@gmail.com>
2008-12-05 21:12:21 +1100

    break index generation and similiarity calc into chunks, simple db storage

--- build_similiar_items.rb
+++ build_similiar_items.rb
@@ -1,64 +1,78 @@
-require 'coeffs'
 require 'read'
-require 'rubygems'
-require 'rbtree'
 require 'movies'
+require 'consts'
+require 'coeffs'
+require 'db'
 
 MIN_NUMBER_OF_COMMON_RATED = 4
 MAX_SIMILIAR_ITEMS = 3
-DATA_DIR = 'test_data_small'
 
-puts "parsing movie info"
-movies_info = Read.all_movie_info
-movies_ids = movies_info.keys
 
-puts "building movie => user => rating hash"
-movie_user_rating = {}
-movies_ids.each_with_index do |mid,idx|
-	puts "parse mid #{mid} (#{idx}/#{movies_ids.length})"
-	movie_user_rating[mid] = Read.movie_ratings mid
-end
 
-movies = Movies.new
-(0...movies_ids.size).each do |i|
-	mid = movies_ids[i]
-	puts "process mid #{mid} (#{i}/#{movies_ids.size})"	
-	m_all_ratings = movie_user_rating[mid]
-	(i+1...movies_ids.size).each do |j|	
-#		puts "-" * 30		
-		omid = movies_ids[j]
-#		puts "mid=#{mid} omid=#{omid}"
-		om_all_ratings = movie_user_rating[omid]
-#		puts "m_all_ratings=#{m_all_ratings.inspect}"
-#		puts "om_all_ratings=#{om_all_ratings.inspect}"
-		m_ratings, om_ratings = Coeff.common m_all_ratings, om_all_ratings
-#		next if u_ratings.size < MIN_NUMBER_OF_COMMON_RATED
-#		puts "m_ratings=#{m_ratings.inspect}"
-#		puts "om_ratings=#{om_ratings.inspect}"
-		pearson_coeff = Coeff.pearson_coeff m_ratings, om_ratings
-#		puts "pearson_coeff = #{pearson_coeff}"
-		movies.add_similiarity mid, omid, pearson_coeff
+class Similiarity 
+	def read_index i1, i2
+		@idx1, @idx2 = [i1,i2].collect do |idx_num|
+			idx_file_name = "#{INDEX_FILE_PREFIX}_#{idx_num}_*.dat"
+			idx_files = `ls #{idx_file_name}`.split "\n"
+			raise "no idx file for #{idx_file_name}" if idx_files.empty?
+			raise "more than one file matched #{idx_file_name}" if idx_files.size > 1
+			hash = Serialise.read idx_files.first
+			puts "idx_file_name #{idx_file_name} hash #{hash.inspect}"
+			hash
+		end
+	end
+
+	def build_similiarity
+#		@movies = Movies.new
+		db = Db.new()
+		@idx1.keys.each do |k1|
+			m1_all_ratings = @idx1[k1]
+			@idx2.keys.each do |k2|
+				next unless k1 < k2
+				m2_all_ratings = @idx2[k2]
+				m1_ratings, m2_ratings = Coeff.common m1_all_ratings, m2_all_ratings
+				pearson_coeff = Coeff.pearson_coeff m1_ratings, m2_ratings
+				db.update k1, k2, pearson_coeff
+				puts "idx1key #{k1} idx2key #{k2} pearson_coeff #{pearson_coeff}"				
+			end
+		end
+		db.close()
+	end
+
+	def dot_output
+		edges = [] # [ n1, n2, sim ]
+		m = @movies.movies
+		m.keys.each do |node|
+			m[node].similiar.to_a.each do |sim_onode|
+				# todo add set here so only add  one way (ie if added a->b don't add b->a)
+				sim,onode = sim_onode
+				edges << [ node, onode, sim ]
+			end
+		end
+
+		# todo: normalise to 0 -> 1 (ie shift smallest to 0 and scale out to 1)
+
+		puts <<-EOF
+		graph G {
+		graph [splines=true overlap=false]
+		EOF
+
+		movies_info = Read.all_movie_info 
+		edges.each do |e| 
+			printf "\"#{movies_info[e[0]]}\" -- \"#{movies_info[e[1]]}\" [label=\"%0.2f\"];\n", (e[2]+1)/2
+		end
+		puts "}"
 	end
 end
 
-edges = [] # [ n1, n2, sim ]
-m = movies.movies
-m.keys.each do |node|
-	m[node].similiar.to_a.each do |sim_onode|
-		# todo add set here so only add  one way (ie if added a->b don't add b->a)
-		sim,onode = sim_onode
-		edges << [ node, onode, sim ]
-	end
+if ARGV.length==2
+	IDX1, IDX2 = ARGV
+else
+	IDX1 = IDX2 = 1
 end
 
-# todo: normalise to 0 -> 1 (ie shift smallest to 0 and scale out to 1)
-
-puts <<EOF
-graph G {
-graph [splines=true overlap=false]
-EOF
-edges.each do |e| 
-	printf "\"#{movies_info[e[0]]}\" -- \"#{movies_info[e[1]]}\" [label=\"%0.2f\"];\n", (e[2]+1)/2
-end
-puts "}"
+similiarity = Similiarity.new
+similiarity.read_index IDX1, IDX2
+similiarity.build_similiarity
+#similiarity.dot_output
 
--- movies.rb
+++ movies.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
+require 'rbtree'
+
 class Movies
 	attr_reader :movies
 
