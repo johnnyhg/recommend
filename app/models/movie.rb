f2d8ab2b5aee936e97e44eb0bf4cec041659f1d0
mat <matthew.kelcey@gmail.com>
2009-03-31 21:22:51 +1100

    add review model, binding a movie to a 1 to 5 value

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
 
+    has_many :reviews
+
     def self.with_most_reviews offset=1
         Movie.find(:first, :order => 'num_reviews DESC', :offset=>offset)
     end
