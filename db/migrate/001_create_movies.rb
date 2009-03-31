f2b1cdc436380b9e30a7bdd558363943d6dc95ab
mat <matthew.kelcey@gmail.com>
2009-03-31 20:58:18 +1100

    add model obj, remake schema

--- db/migrate/001_create_movies.rb
+++ db/migrate/001_create_movies.rb
@@ -1,14 +1,14 @@
 class CreateMovies < ActiveRecord::Migration
   def self.up
-    create_table "movies", :force => true do |t|
+    create_table :movies do |t|
       t.column "name", :string
       t.column "num_reviews", :integer
+      t.column "year", :integer
       t.timestamps
     end
   end
-  
-  def self.down
-    drop_table "movies"
-  end
 
+  def self.down
+    drop_table :movies
+  end
 end
