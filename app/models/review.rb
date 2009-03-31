9e6b0266cd33051cb28e5d71169e28e66a1ea143
mat <matthew.kelcey@gmail.com>
2009-03-31 21:25:30 +1100

    include validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ActiveRecord::Base
     belongs_to :movie
+    validate :review_between_1_and_5
+
+    def review_between_1_and_5
+        errors.add_to_base "review should be >=1 and <=5 but is #{review}" unless review>=1 && review<=5
+    end
+
 end
