1c9f9bd9e77900aa4981e8de5edd42575fb30b2f
mat <matthew.kelcey@gmail.com>
2009-04-01 23:34:48 +1100

    review model and simple ui for reviews as a resource

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+
+    def index
+        @reviews = Review.find(:all)
+    end
+
+    def new
+        @next_movie_to_review = Movie.next_movie_to_review
+        redirect_to :action => :index if @next_movie_to_review.nil?
+    end
+
+    def create        
+        movie  = Movie.find(params['review']['movie'])
+        rating = params['review']['rating']
+        puts "movie #{movie} rating #{rating}" 
+        Review.create! :movie => movie, :rating => rating
+        redirect_to :action => :new
+    end
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,18 @@ class Movie < ActiveRecord::Base
     has_many :reviews
 
     def self.with_most_reviews offset=1
-        Movie.find(:first, :order => 'num_reviews DESC', :offset=>offset)
+        Movie.find(:first, :order => 'num_reviews desc, id asc', :offset=>offset)
+    end
+
+    def self.next_movie_to_review
+        offset = 0
+        while true do
+            @next_movie_to_review = Movie.with_most_reviews offset
+            puts "NMTR #{@next_movie_to_review}"
+            return nil if @next_movie_to_review.nil?
+            return @next_movie_to_review if Review.find_by_movie_id(@next_movie_to_review.id).nil?
+            offset += 1
+        end
     end
 
 end
