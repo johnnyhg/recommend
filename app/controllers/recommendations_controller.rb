e5b7ae082cd18c62828699c0ad793672d267550e
mat <matthew.kelcey@gmail.com>
2009-04-04 15:55:07 +1100

    rake task to run if data dir changed, fix in recommendation engine to include recommendations for those reviewed as unseen

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,14 +1,22 @@
 class RecommendationsController < ApplicationController
 
     def index
-        reviews = Review.find(:all, :conditions => 'rating>0')
-        if reviews.size==0            
+        
+        if Review.count==0
             @recommendations=[]
             return
         end
-        unseen_movies = Movie.find(:all).select { |m| Review.find_by_movie_id(m.id).nil? }
-        puts "reviews given #{reviews.collect{|r| r.movie.name}.inspect}"
-        puts "unseen_movies #{unseen_movies.collect(&:name).inspect}"
+
+        reviews = Review.find(:all, :conditions => 'rating>0')
+        
+        movies_declared_unseen = Review.find(:all, :conditions => 'rating=0').collect { |r| r.movie }
+        movies_totally_unknown = Movie.find(:all).select { |m| Review.find_by_movie_id(m.id).nil? }         
+        unseen_movies = movies_declared_unseen + movies_totally_unknown
+
+        puts "reviews given #{reviews.collect{|r| r.movie.name}.sort.inspect}"
+        puts "movies_declared_unseen #{movies_declared_unseen.collect(&:name).sort.inspect}"
+        puts "movies_totally_unknown #{movies_totally_unknown.collect(&:name).sort.inspect}"
+
         @recommendations = unseen_movies.collect do |unseen_movie|
             similarities_sum = 0
             similarities_x_review_sum = 0
@@ -17,11 +25,11 @@ class RecommendationsController < ApplicationController
                 similarities_sum += similarity
                 similarities_x_review_sum += similarity * review.rating
 
-                puts "similarity of #{unseen_movie.name} to #{review.movie.name} "+
-                        "rating (#{review.rating}) "+             
-                        "is #{similarity}. "+
-                        "similarities_sum=#{similarities_sum}, " +
-                        "similarities_x_review_sum=#{similarities_x_review_sum}"
+#                puts "similarity of #{unseen_movie.name} to #{review.movie.name} "+
+#                        "rating (#{review.rating}) "+
+#                        "is #{similarity}. "+
+#                        "similarities_sum=#{similarities_sum}, " +
+#                        "similarities_x_review_sum=#{similarities_x_review_sum}"
                 
             end
             expected_rating = similarities_x_review_sum / similarities_sum
