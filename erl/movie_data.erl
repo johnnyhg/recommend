e82fc77613cb7a8f4eeb5f65ddf6a816116c1abc
mat <matthew.kelcey@gmail.com>
2009-04-16 21:58:54 +1000

    testing timing of single, parallel in chunks and pmap

--- erl/movie_data.erl
+++ erl/movie_data.erl
@@ -1,8 +1,37 @@
 -module(movie_data).
--export([start/0,stop/0,ids/0,title/1,ratings/1,populate_tables/0,test/0]).
+-export([start/0,stop/0,ids/0,ratings/1,populate_tables/0,
+	 write_movie_rating/1,
+	 test_serial/0,test_pmap/0,test_parallel/1,
+	 write_movie_ids/0,write_movie_ratings_pmap/0,write_movie_ratings_parallel/1,
+	 reset_ids/0]).
 %-compile(export_all).
 -include_lib("consts.hrl").
 
+test_serial() ->
+    test(fun write_movie_ratings_serial/0).
+
+test_pmap() ->
+    test(fun write_movie_ratings_pmap/0).
+
+test_parallel(Args) ->
+    NumProcs = list_to_integer(hd(Args)),
+    io:format("running with ~w, ",[NumProcs]),
+    test(fun() -> write_movie_ratings_parallel(NumProcs) end).
+		  
+test(F) ->    
+    Start = now(),
+    start(),
+    F(),
+    io:format("~p s, ",[timer:now_diff(now(),Start)/1000/1000]),
+    Sizes = [ length(ratings(M)) || M <- ids() ],
+    io:format("reviews ~w\n", [lists:sum(Sizes)]),
+    stop().
+    
+reset_ids() ->
+    start(),
+    dets:delete_all_objects(?TITLES),
+    write_movie_ids(),
+    io:format("~w~n",[length(ids())]).
 
 start() ->
     dets:open_file(?TITLES, [{file,"movie_titles.dets"}]),
@@ -18,15 +47,12 @@ ids() ->
     % dets annoying wraps even set matches in a list
     [ Id || [{Id}] <- dets:match(?TITLES, '$1')].
 
-title(Id) -> 
-    util:extract_value(dets:lookup(?TITLES, Id)).
-
 ratings(Id) -> 
     util:extract_value(dets:lookup(?RATINGS, Id)).
 
 populate_tables() ->
     write_movie_ids(),
-    write_movie_ratings().
+    write_movie_ratings_serial().
 
 write_movie_ids() ->
     {ok,B} = file:read_file(?PATH ++ "/movie_titles.txt"),
@@ -39,15 +65,21 @@ write_movie_ids() ->
       end, 
       Lines).
 
-write_movie_ratings() ->
-    lists:foreach(
-      fun(Id) -> write_movie_rating(Id) end,
-      ids()
-     ).
+% single process (this one) for all ratings files
+write_movie_ratings_serial() ->
+    lists:foreach(fun(Id) -> write_movie_rating(Id) end, ids()).
+
+% a process for each rating file
+write_movie_ratings_pmap() ->
+    rpc:pmap({?MODULE,write_movie_rating}, [], ids()).
+    
+% rating files across NumProcesses processes 
+write_movie_ratings_parallel(NumProcesses) ->
+    util:parallel_eval_in_chunks(?MODULE,write_movie_rating,NumProcesses,ids()).
     
 write_movie_rating(MovieId) -> 
     Filename = ?PATH ++ "/training_set/mv_" ++ padded(MovieId,7) ++ ".txt",
-    io:format("file is ~p~n",[Filename]),
+    %io:format("file is ~p~n",[Filename]),
     {ok,B} = file:read_file(Filename),    
     Lines = string:tokens(binary_to_list(B), "\n"),
     ConvertALine = fun([UserId,Rating,_Date]) -> { list_to_integer(UserId), list_to_integer(Rating) } end,
--- erl/util.erl
+++ erl/util.erl
@@ -1,5 +1,5 @@
 -module(util).
--export([extract_value/1,split/2]).
+-export([extract_value/1,split/2,parallel_eval_in_chunks/4,apply_all/3,next_line/1]).
 %-compile(export_all).
 
 extract_value(Record) ->
@@ -11,6 +11,17 @@ extract_value(Record) ->
 	    Value
     end.
 
+% apply Module:Function to each elem of List run across NumChunks processes 
+% NumChunks << length(List)
+parallel_eval_in_chunks(Module,Function,NumChunks,List) ->
+    Chunks = split(List,NumChunks),  
+    %io:format("split ~w\n",[Split]),
+    Calls = [ {?MODULE,apply_all,[Module,Function,Chunk]} || Chunk <- Chunks ],
+    rpc:parallel_eval(Calls).
+
+apply_all(Module,Function,List) ->
+    lists:foreach(fun(X) -> apply(Module,Function,[X]) end, List).  
+
 % split List into N chunks
 split(List,N) -> 
     Chunks = [ [] || _X <- lists:seq(1,N)],
@@ -21,6 +32,15 @@ split(List,[],Acc) ->
     split(List,Acc,[]);
 split([H|T],[CH|CT],Acc) ->
     split(T,CT,[[H|CH]|Acc]).
+
+next_line(Binary) ->
+    next_line(Binary, []).
+next_line(<<>>, _Collected) ->
+    ignore_last_line_if_didnt_end_in_newline;
+next_line(<<"\n",Rest/binary>>, Collected) ->  
+    { Rest, binary_to_list(list_to_binary(lists:reverse(Collected))) }; % black magic voodoo line
+next_line(<<C:1/binary,Rest/binary>>, Collected) ->
+    next_line(Rest, [C|Collected]). 
 
 
 
