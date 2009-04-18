0139038f4f38da6907ae981289d37fdebc38ec64
mat <matthew.kelcey@gmail.com>
2009-04-19 09:15:31 +1000

    mnesia

--- erl/similarity.erl
+++ erl/similarity.erl
@@ -10,17 +10,17 @@ stop() ->
     movie_data:stop().
     
 get(M1,M2)->
-    Similarity = similarity_store:get({M1,M2}),
+    Similarity = similarity_store:get(M1,M2),
     case Similarity of
 	record_not_found ->
-	    io:format("~p ~p ~p not cached~n",[self(),M1,M2]),
+	    %io:format("~p ~p ~p not cached~n",[self(),M1,M2]),
 	    Ratings1 = movie_data:ratings(M1),
 	    Ratings2 = movie_data:ratings(M2),
 	    Calculated = coeff:pearson(Ratings1, Ratings2),
 	    similarity_store:set({M1,M2},Calculated),
 	    Calculated;
 	_ ->
-	    io:format("~p ~p ~p cached~n",[self(),M1,M2]),
+	    %io:format("~p ~p ~p cached~n",[self(),M1,M2]),
 	    Similarity
     end.
 
@@ -32,12 +32,10 @@ is_cached(M1,M2) ->
     end.
 
 calc_all_for(Mid) ->	        
-    start(),
     lists:foreach(
-      fun(OtherId) -> get(Mid,OtherId) end,
+      fun(OtherId) -> get({Mid,OtherId}) end,
       movie_data:ids()
-     ),
-    stop().
+     ).
     
 	    
 
--- erl/similarity_store.erl
+++ erl/similarity_store.erl
@@ -1,18 +1,41 @@
 -module(similarity_store).
 -compile(export_all).
+-include("records.hrl").
+-include_lib("stdlib/include/qlc.hrl").
 
 start() ->
-    dets:open_file(?MODULE,[{file,"similarity.dets"}]), ok.
+    mnesia:start().
     
 stop() ->
-    dets:close(?MODULE), ok.
+    mnesia:stop().
 
-set({_,_}=Pair,Similarity) ->    
-    dets:insert(?MODULE,{ensure_a_less_than_b(Pair),Similarity}).
+delete_all() ->
+    start(),
+    mnesia:clear_table(similarity).
+
+bootstrap() ->
+    mnesia:create_schema([node()]),    
+    start(),
+    mnesia:create_table(similarity, 
+			[{attributes, record_info(fields, similarity)}, 
+			{type,set}, 
+			{disc_copies, [node()]}]).
+
+set({Id1,Id2},Similarity) when Id1 > Id2 ->    
+    set({Id2,Id1},Similarity);
+set({Id1,Id2},Similarity) ->
+    R = #similarity{idPair={Id1,Id2},similarity=Similarity},
+    %mnesia:transaction(fun() -> mnesia:write(R) end).
+    mnesia:dirty_write(R).
   
-get({_,_}=Pair) ->
-    util:extract_value(dets:lookup(?MODULE,ensure_a_less_than_b(Pair))).
+get({Id1,Id2}) when Id1 > Id2 ->
+    similarity_store:get({Id2,Id1});
+get({Id1,Id2}) ->
+    %{atomic,Result} = mnesia:transaction(fun() -> mnesia:read(similarity, {Id1,Id2}, read) end),
+    Result = mnesia:dirty_read(similarity, {Id1,Id2}),
+    case length(Result) of
+	0 -> record_not_found;
+	_ -> (hd(Result))#similarity.similarity
+    end.	     
     
-ensure_a_less_than_b({A,B}) when A<B -> {A,B};
-ensure_a_less_than_b({A,B}) ->          {B,A}.
 
